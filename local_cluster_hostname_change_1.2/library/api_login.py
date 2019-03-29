b952f206a7e359d666fa63917797a78aee25bfcb
Lucian Bucur <lucian.bucur@solarwindsits-MacBook-Pro-2.local>
2019-03-29 17:22:22 +0200

    update api_login

--- local_cluster_hostname_change_1.2/library/api_login.py
+++ local_cluster_hostname_change_1.2/library/api_login.py
@@ -4,9 +4,10 @@ import requests
 import json
 from ansible.module_utils.basic import *
 
-def get_appkey(module,api_url,username,password):
+def get_appkey(module,api_url,username,password,username_auth,password_auth):
     import requests
     import json
+    from requests.auth import HTTPBasicAuth
     headers = {'Content-Type': 'application/json'}
     payload = {
          "id": "1",
@@ -18,7 +19,7 @@ def get_appkey(module,api_url,username,password):
          }
     }
     try:
-       response         = requests.get(api_url,headers=headers,data=json.dumps(payload))
+       response         = requests.get(api_url,headers=headers,data=json.dumps(payload),auth=HTTPBasicAuth(username_auth, password_auth))
        response_content = json.loads(response.content)
        return response_content['result']
     except Exception as e:
@@ -26,9 +27,11 @@ def get_appkey(module,api_url,username,password):
 
 def main():
     arguments = {
-        "api_url"   : { "required": True, "type": "str"                 },
-        "username"  : { "required": True, "type": "str"                 },
-        "password"  : { "required": True, "type": "str", "no_log": True }
+        "api_url"        : { "required": True, "type": "str"                 },
+        "username"       : { "required": True, "type": "str"                 },
+        "password"       : { "required": True, "type": "str", "no_log": True },
+        "username_auth"  : { "required": True, "type": "str"                 },
+        "password_auth"  : { "required": True, "type": "str", "no_log": True }
     }
     module = AnsibleModule(
         argument_spec=arguments,
@@ -38,7 +41,9 @@ def main():
     api_url  = module.params['api_url']
     username = module.params['username']
     password = module.params['password']
-    appkey   = get_appkey(module,api_url,username,password)
+    username_auth = module.params['username_auth']
+    password_auth = module.params['password_auth']
+    appkey   = get_appkey(module,api_url,username,password,username_auth,password_auth)
 
     if module.check_mode:
         module.exit_json(changed=True)
