71d6fbd03777fb5123deea443eed43ec00404096
Lucian Bucur <lucian.bucur@solarwindsits-MacBook-Pro-2.local>
2019-03-29 10:20:35 +0200

    server_decommision

--- local_cluster_hostname_change_1.2/library/api_login.py
+++ local_cluster_hostname_change_1.2/library/api_login.py
@@ -3,48 +3,31 @@
 import requests
 import json
 
-def get_appkey(module,api_url,username,password):
-    import requests
-    import json
-    headers = {'Content-Type': 'application/json'}
-    payload = {
-         "jsonrpc": "2.0",
-         "method": "user.login",
-         "params": {
-             "user":     username,
-             "password": password
-         },
-         "id": 1
-    }
-    try:
-           response         = requests.get(api_url,headers=headers,json=payload)
-           response_content = json.loads(response.content)
-           return response_content['result']
-    except Exception as e:
-        module.fail_json(msg="Error! code={}:message={}".format(type(e).__name__,str(e)))
-
-
 def main():
-    arguments = dict(
-            api_url  = dict(required=True),
-            username = dict(required=True),
-            password = dict(required=True, no_log=True)
-        )
+    arguments = {
+            "api_url"   : { "required": True,   "type": "str" },
+            "payload"   : { "required": "True", "type":"json" }
+    }
     module = AnsibleModule(
         argument_spec=arguments,
         supports_check_mode=True
     )
 
-    api_url=module.params['api_url']
-    username=module.params['username']
-    password=module.params['password']
-    appkey=get_appkey(module,api_url,username,password)
+    api_url = module.params['api_url']
+    payload = module.params['payload']
+
+    headers    = {'Content-Type': 'application/json'}
+    try:
+        response = requests.get(api_url,headers=headers,data=payload)
+        #appkey    = json.loads(response.content)['result']
+    except Exception as e:
+           module.fail_json(msg="Error! code={}:message={}".format(type(e).__name__,str(e)))
 
     if module.check_mode:
         module.exit_json(changed=True)
 
     if not module.check_mode:
-        module.exit_json(changed=True, appkey='{}'.format(appkey))
+        module.exit_json(changed=True, appkey='{}'.format(response.content))
 
 
 from ansible.module_utils.basic import *
