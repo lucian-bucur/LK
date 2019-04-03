884caf8688a03fcd531abf455c814f7c0eeeedb3
Lucian Bucur <lucian.bucur@solarwindsits-MacBook-Pro-2.local>
2019-04-03 18:15:52 +0300

    update checks

---
+++ testx/files/reports.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import requests
+import json
+from subprocess import Popen,PIPE,STDOUT,call
+
+import sys
+def dumpx(ax,bx,key='hostname'):
+    dx = {}
+    for hostx in ax:
+        for v_address in bx:
+              command="/usr/bin/host" + ' ' + str(hostx) + '| grep ' + str(v_address)
+              proc=Popen(command, shell=True, stdout=PIPE, )
+              output=proc.communicate()[0]
+              if output != '':
+                 if key=='hostname':
+                    if hostx not in dx.keys(): dx[hostx] = []
+                    dx[hostx].append(v_address)
+                 if key=='address':
+                    if v_address not in dx.keys(): dx[v_address] = []
+                    dx[v_address].append(hostx)
+    return dx
+
+
+print sys.argv[1]
+print sys.argv[2]
+
+
+# dumpx(ax,bx)
+
+
+# dumpx(['server1.test57.simplyspamfree.com'],['94.130.122.232'])
