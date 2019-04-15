4d8e371cfd5db1dd60d926ed5a0f6ee56c73297a
Lucian Bucur <lucian.bucur@solarwindsits-MacBook-Pro-2.local>
2019-04-15 10:03:06 +0300

    k

---
+++ migrations/library/get_host_groups.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from ansible.module_utils.basic import *
+from ansible.module_utils.cluster_api_tools import get_host_groups
+
+
+def main():
+    fields = {
+        "host": {"default": True, "type": "str"},
+    }
+
+    module = AnsibleModule(argument_spec=fields)
+    host= module.params["host"]
+    result = get_host_groups(host)
+
+    module.exit_json(changed=True, meta=result)
+
+
+if __name__ == '__main__':
+    main()
