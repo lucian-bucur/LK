6ff891bfec61adbce062618670757e202fb10208
Lucian Bucur <lucian.bucur@solarwindsits-MacBook-Pro-2.local>
2019-04-22 13:56:35 +0300

    update LK

---
+++ ansible/move_logging_domain_1.4/module_utils/test.py
@@ -0,0 +1,6 @@
+from cluster_api_service import ClusterApiService
+
+
+service = ClusterApiService("test57.simplyspamfree.com")
+service.get_cluster_conf()
+# service.add_imap_servers("server1.test57.simplyspamfree.com")
